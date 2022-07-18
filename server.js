86ae58d6f30efc9cfdeaf7856c8e76077e9fa748
Vadad Rzali <vedad.rzali@gmail.com>
2022-07-17 22:15:46 -0400

    Add POST code

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 const { animals } = require('./data/animals');
 
 const express = require('express');
 const PORT = process.env.PORT || 3002;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`API Server now on port ${PORT}!`);
@@ -55,6 +63,32 @@ function findById(id, animalsArray) {
     return result;
 };
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+  };
+
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -70,5 +104,18 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
-    
 });
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('Animal is not properly formatted.');
+    } else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+    }
+  });
+
